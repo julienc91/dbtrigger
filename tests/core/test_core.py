d06916a019641377bf3d45b2e8e38399643450db
julien <git@julienc.io>
2018-10-28 20:42:06 +0100

    improving code coverage

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -4,11 +4,15 @@ import pytest
 
 from dbtrigger.core import run
 from dbtrigger.core.sqlite import SqliteRunner
+from dbtrigger.core.postgresql import PostgresqlRunner
+from dbtrigger.core.mysql import MysqlRunner
 from dbtrigger.models import Dialect
 
 
 @pytest.mark.parametrize('dialect, expected', [
     (Dialect.sqlite, SqliteRunner),
+    (Dialect.postgresql, PostgresqlRunner),
+    (Dialect.mysql, MysqlRunner),
 ])
 def test_run(monkeypatch, query, dialect, expected):
     query.database.server.dialect = dialect
